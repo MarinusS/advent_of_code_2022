1a15b3d0e819d76f1e58f6b89e808c528cd6584e
Marinus Sewalt <marinus.sewalt@gmail.com>
2023-05-22 14:58:32 +0800

    Day6 code clean up

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -12,7 +12,7 @@ fn search_for_marker(input: &str, marker_size: usize) -> Option<usize> {
         //That means we can assign each characters to a bit in a u32
         //We can the do an or for between all the characters and the number of ones will be the
         //number of unique characters
-        let ones = slice.iter().map(|byte| (1 as u32) << (byte - 'a' as u8));
+        let ones = slice.iter().map(|byte| 1_u32 << (byte - b'a'));
         let number_of_ones: u32 = ones.fold(0, |acc: u32, byte: u32| acc | byte).count_ones();
 
         if number_of_ones == marker_size as u32 {
@@ -28,7 +28,7 @@ fn get_answer_a(input: &str) -> Option<usize> {
     search_for_marker(input, START_OF_PACKET_SIZE)
 }
 
-fn get_answer_b(input: &String) -> Option<usize> {
+fn get_answer_b(input: &str) -> Option<usize> {
     const START_OF_MESAGE_SIZE: usize = 14;
     search_for_marker(input, START_OF_MESAGE_SIZE)
 }
@@ -49,20 +49,20 @@ mod tests {
     #[test]
     fn test_get_answer_a() {
         assert_eq!(
-            get_answer_a(&("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string())),
+            get_answer_a("bvwbjplbgvbhsrlpgdmjqwftvncz"),
             Some(5)
         );
 
         assert_eq!(
-            get_answer_a(&("nppdvjthqldpwncqszvftbrmjlhg".to_string())),
+            get_answer_a("nppdvjthqldpwncqszvftbrmjlhg"),
             Some(6)
         );
         assert_eq!(
-            get_answer_a(&("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string())),
+            get_answer_a("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
             Some(10)
         );
         assert_eq!(
-            get_answer_a(&("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string())),
+            get_answer_a("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
             Some(11)
         );
     }
@@ -70,24 +70,24 @@ mod tests {
     #[test]
     fn test_get_answer_b() {
         assert_eq!(
-            get_answer_b(&("mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_string())),
+            get_answer_b("mjqjpqmgbljsphdztnvjfqwrcgsmlb"),
             Some(19)
         );
 
         assert_eq!(
-            get_answer_b(&("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string())),
+            get_answer_b("bvwbjplbgvbhsrlpgdmjqwftvncz"),
             Some(23)
         );
         assert_eq!(
-            get_answer_b(&("nppdvjthqldpwncqszvftbrmjlhg".to_string())),
+            get_answer_b("nppdvjthqldpwncqszvftbrmjlhg"),
             Some(23)
         );
         assert_eq!(
-            get_answer_b(&("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string())),
+            get_answer_b("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
             Some(29)
         );
         assert_eq!(
-            get_answer_b(&("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string())),
+            get_answer_b("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
             Some(26)
         );
     }
